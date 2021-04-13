954cc0e7b14b4ad0a4e7aee80145fb05596daae4
Payton Swick <payton@foolord.com>
2021-04-13 18:20:29 -0400

    Add encodeProductForUrl/decodeProductFromUrl to wpcom-checkout

--- packages/wpcom-checkout/src/index.ts
+++ packages/wpcom-checkout/src/index.ts
@@ -5,5 +5,6 @@ import useDisplayCartMessages from './use-display-cart-messages';
 
 export * from './transformations';
 export * from './types';
+export * from './product-url-encoding';
 export { useDisplayCartMessages };
 export { createApplePayMethod } from './payment-methods/apple-pay';
